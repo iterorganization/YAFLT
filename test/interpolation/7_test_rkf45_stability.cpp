17b8f502cfaef381927790bfe2aa8a9377974bb5
Gregor Simič <gregor.simic@iter.org>
2023-07-18 14:34:49 +0200

    Finished test 7 that outputs rkf45 data when following a single magnetic surface for a lot of revolutions. The output, if directed to a file, can be plotted to display the stability of the solver.

--- test/interpolation/7_test_rkf45_stability.cpp
+++ test/interpolation/7_test_rkf45_stability.cpp
@@ -1,7 +1,33 @@
-//TODO
+/*
+Example showing how to use the RKF45 solver. Also it shows the stability by
+following a single magnetic surface for quite some time.
+
+It prints the Phi, R, Z values as well as the relative error of the flux value,
+where the initial flux value is compared as reference.
+
+One can plot the contents simply with
+
+.. code-block:: python
+
+   phi = []
+   r = []
+   z = []
+   with open("OUT", "r") as f:
+      for line in f:
+          sline = line.split()
+          phi.append(float[sline[0]])
+          r.append(float[sline[1]])
+          z.append(float[sline[2]])
+
+   import matplotlib.pyplot as plt
+   plt.plot(r, z)
+   plt.show()
+*/
+
 #include <eqdsk_drsep10_data.hpp>
 #include <rkf45.hpp>
 #include <bicubic.hpp>
+#include <cstdio>
 int main(){
 
     RKF45 *solver = new RKF45();
@@ -24,6 +50,42 @@ int main(){
     }
     naive_interp->prepareContainers();
     naive_interp->setArrays(drsep10_Rs, drsep10_Zs, reshaped_Psi);
+    double drsep10_fpolvacuum = -32.86;
+    solver->set_vacuum_fpol(drsep10_fpolvacuum);
+
+    // Set the initial parameters. And follow a fieldline for absurd amount
+    // of distance
+
+    // Containers for the position and derivative
+    double y[2], yp[2];
+    double time=0, new_time=0, time_step=0.01;
+    // Flag=2 sets the solver in a single step-mode.
+    int flag=2;
+
+    // Desired accuracy.
+    double relerr=1e-4, abserr=1e-4;
+    y[0] = 6.4;
+    y[1] = 0.0;
+
+    double dummy, flux, new_flux;
+    // Get the initial flux value to compare on the new locations.
+    naive_interp->getValues(y[0], y[1],  flux, dummy, dummy);
+    unsigned int counter=0;
+    unsigned int N=1000000;
+    while (counter < N){
+        new_time = time + time_step;
+        flag = solver->r8_rkf45(y, yp, &time, new_time, &relerr, abserr, flag);
+        // printf("%f %f %d\n", y[0], y[1], flag);
+        naive_interp->getValues(y[0], y[1],  new_flux, dummy, dummy);
+        dummy = fabs(new_flux - flux);
+        printf("%f %f %f %e\n", time / (2 * 3.14), y[0], y[1], fabs(dummy / flux));
+        // Set the flag back to 2 and ignore other messages from the solver.
+        flag = 2;
+        counter = counter + 1;
+    }
+
+    // Free objects
+    delete solver; // Also deletes the interpolation object tied to it.
 
     return 0;
 }
